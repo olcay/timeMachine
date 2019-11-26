4b334730ee66bacebc3ac6d505f6561d3244682a
Olcay Bayram <olcaybayram@gmail.com>
2019-11-26 22:51:56 +0100

    Change background

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,92 +28,157 @@
 	});
 
 	// Slideshow Background.
-	(function () {
-
+	var BackgroundManager = function () {
 		// Settings.
 		var settings = {
-
 			// Images (in the format of 'url': 'alignment').
 			images: {
-				'images/bg01.jpg': 'center',
+				'images/bg03.jpg': 'center',
 				'images/bg02.jpg': 'center',
-				'images/bg03.jpg': 'center'
+				'images/bg01.jpg': 'center'
 			},
 
 			// Delay.
 			delay: 6000
-
 		};
 
-		// Vars.
-		var pos = 0, lastPos = 0,
-			$wrapper, $bgs = [], $bg,
-			k, v;
+		var isAtDefault = true;
 
-		// Create BG wrapper, BGs.
-		$wrapper = document.createElement('div');
-		$wrapper.id = 'bg';
-		$body.appendChild($wrapper);
+		var init = function () {
+			// Vars.
+			var pos = 0, lastPos = 0,
+				$wrapper, $bgs = [], $bg,
+				k, v;
 
-		for (k in settings.images) {
+			var $wrapper = document.querySelector('#bg');
 
-			// Create BG.
-			$bg = document.createElement('div');
-			$bg.style.backgroundImage = 'url("' + k + '")';
-			$bg.style.backgroundPosition = settings.images[k];
-			$wrapper.appendChild($bg);
+			if ($wrapper) {
+				$wrapper.innerHTML = '';
+			} else {
+				// Create BG wrapper, BGs.
+				$wrapper = document.createElement('div');
+				$wrapper.id = 'bg';
+				$body.appendChild($wrapper);
+			}
 
-			// Add it to array.
-			$bgs.push($bg);
+			for (k in settings.images) {
 
-		}
+				// Create BG.
+				$bg = document.createElement('div');
+				$bg.style.backgroundImage = 'url("' + k + '")';
+				$bg.style.backgroundPosition = settings.images[k];
+				$wrapper.appendChild($bg);
 
-		// Main loop.
-		$bgs[pos].classList.add('visible');
-		$bgs[pos].classList.add('top');
+				// Add it to array.
+				$bgs.push($bg);
 
-		// Bail if we only have a single BG or the client doesn't support transitions.
-		if ($bgs.length == 1
-			|| !canUse('transition'))
-			return;
+			}
 
-		window.setInterval(function () {
-
-			lastPos = pos;
-			pos++;
-
-			// Wrap to beginning if necessary.
-			if (pos >= $bgs.length)
-				pos = 0;
-
-			// Swap top images.
-			$bgs[lastPos].classList.remove('top');
+			// Main loop.
 			$bgs[pos].classList.add('visible');
 			$bgs[pos].classList.add('top');
 
-			// Hide last image after a short delay.
-			window.setTimeout(function () {
-				$bgs[lastPos].classList.remove('visible');
-			}, settings.delay / 2);
+			// Bail if we only have a single BG or the client doesn't support transitions.
+			if ($bgs.length == 1
+				|| !canUse('transition'))
+				return;
 
-		}, settings.delay);
+			window.setInterval(function () {
 
-	})();
+				lastPos = pos;
+				pos++;
+
+				// Wrap to beginning if necessary.
+				if (pos >= $bgs.length)
+					pos = 0;
+
+				// Swap top images.
+				$bgs[lastPos].classList.remove('top');
+				$bgs[pos].classList.add('visible');
+				$bgs[pos].classList.add('top');
+
+				// Hide last image after a short delay.
+				window.setTimeout(function () {
+					$bgs[lastPos].classList.remove('visible');
+				}, settings.delay / 2);
+
+			}, settings.delay);
+		};
+
+		var changeBackgroundImage = function (image) {
+			if (image) {
+				settings.images = {};
+				settings.images['images/' + image] = 'center';
+				isAtDefault = false;
+				init();
+			} else {
+				settings.images = {
+					'images/bg01.jpg': 'center',
+					'images/bg02.jpg': 'center',
+					'images/bg03.jpg': 'center'
+				};
+				if (!isAtDefault) {
+					init();
+				}
+				isAtDefault = true;
+			}
+		};
+
+		init();
+
+		return {
+			changeBackgroundImage: changeBackgroundImage
+		};
+	}();
 
 	// Time Console Form.
-	(function () {
+	var TimeConsole = function (backgroundManager) {
+
+		var dateInputMask = function (elm) {
+			elm.addEventListener('keypress', function (e) {
+				var len = elm.value.length;
+
+				if (len === 10 && e.keyCode == 13) {
+					return;
+				}
+
+				if (e.keyCode < 47 || e.keyCode > 57) {
+					e.preventDefault();
+				}
+
+				// If we're at a particular place, let the user type the slash
+				// i.e., 12/12/1212
+				if (len !== 1 || len !== 3) {
+					if (e.keyCode == 47) {
+						e.preventDefault();
+					}
+				}
+
+				// If they don't add the slash, do it for them...
+				if (len === 2) {
+					elm.value += '/';
+				}
+
+				// If they don't add the slash, do it for them...
+				if (len === 5) {
+					elm.value += '/';
+				}
+			});
+		};
 
 		// Vars.
 		var $form = document.querySelectorAll('#time-console-form')[0],
 			$submit = document.querySelectorAll('#time-console-form input[type="submit"]')[0],
-			$date = document.querySelectorAll('#time-console-form input[type="date"]')[0],
+			$date = document.querySelectorAll('#time-console-form input[type="text"]')[0],
 			$visor = document.querySelector('#visor'),
 			$xmlhttp = new XMLHttpRequest(),
 			$url = "data.json",
 			$message;
 
+		dateInputMask($date);
+
 		// Set date to today
-		$date.valueAsDate = new Date();
+		$date.value = new Date().toLocaleDateString();
 
 		// Bail if addEventListener isn't supported.
 		if (!('addEventListener' in $form))
@@ -147,7 +212,6 @@
 
 		// Events.
 		$form.addEventListener('submit', function (event) {
-
 			event.stopPropagation();
 			event.preventDefault();
 
@@ -163,25 +227,32 @@
 					if (this.status == 200) {
 						var eventArray = JSON.parse(this.responseText);
 
-						var beforeEvent, afterEvent, visorText;
+						var beforeEvent, afterEvent, visorText, destination = new Date($date.value).setHours(1);
 
 						var i;
 						for (i = 0; i < eventArray.length; i++) {
-							if (new Date(eventArray[i].date) <= $date.valueAsDate) {
+							if (new Date(eventArray[i].date) <= destination) {
 								beforeEvent = eventArray[i];
 								afterEvent = eventArray[i - 1];
 								break;
 							}
 						}
 
+						var isAfterTheEvent = false;
+
 						if (beforeEvent) {
 							visorText = beforeEvent.afterText;
+							isAfterTheEvent = true;
 						} else {
 							visorText = eventArray[eventArray.length - 1].beforeText;
 						}
 
-						if (afterEvent && ($date.valueAsDate - new Date(beforeEvent.date) > new Date(afterEvent.date) - $date.valueAsDate)) {
+						if (afterEvent && (destination - new Date(beforeEvent.date) > new Date(afterEvent.date) - destination)) {
 							visorText = afterEvent.beforeText;
+						}
+
+						if (isAfterTheEvent) {
+							backgroundManager.changeBackgroundImage(beforeEvent.image);
 						}
 
 						// Visor update.
@@ -203,6 +274,10 @@
 
 		});
 
-	})();
+
+
+	}(BackgroundManager);
+
+
 
 })();
